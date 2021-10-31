27356424b598db8ee503dc71a7a6b99b2b7e3761
abolfazl-hue <abolfazljdst@gmail.com>
2021-10-31 16:04:51 +0330

    fetch api data with a plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,9 @@ export default {
     css: ["~/assets/main.scss"],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
+    plugins: [
+        '~/plugins/getData.server.js'
+    ],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
