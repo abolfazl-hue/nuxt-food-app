68eba26476fee74fcd7e68a8abd2456bd81b40f1
abolfazl-hue <abolfazljdst@gmail.com>
2021-11-03 15:01:32 +0330

    create a food filtering for app restaurant

---
+++ components/AppSelect.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <label for="food">Wich is your favorite food ?</label>
+
+      <select name="food" id="food" v-model="selectedFood">
+          <option value disabled>-- please select a food !</option>
+          <option v-for="option in selectedOptions" :key="option" :value="option">{{option}}</option>
+      </select>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedOptions: ['tacos', 'pizza', 'dem sum'],
+            selectedFood : ''
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="container restaurants">
     <h1>Restaurants</h1>
+
+    <AppSelect/>
     <AppRestaurantInfo/>
   </main>
 </template>
