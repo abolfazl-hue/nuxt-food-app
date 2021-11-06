51482c75a3556ace6513af8de16a52f66f294703
abolfazl-hue <abolfazljdst@gmail.com>
2021-11-06 12:52:44 +0330

    Add icon with conditional layout

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -2,7 +2,9 @@
   <nav>
       <ul>
           <li>
-              <nuxt-link to="/">Home</nuxt-link>
+              <nuxt-link to="/">
+                    <AppLogo/>
+              </nuxt-link>
           </li>
           <li>
               <nuxt-link to="/restaurants">Restaurants</nuxt-link>
